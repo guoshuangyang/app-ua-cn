93a3d8bf576dcb8a4a09d58b67493733cf454432
gsy <2321756533@qq.com>
2024-01-30 18:57:00 +0800

    fix: iife修复导入异常，打包导致window并无改方法

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 // 根据window的ua判断是否是移动端以及是什么app内部
+import * as whatUa from "./is";
 export * from "./is";
-
 // if in browser
 if (typeof window !== "undefined") {
   // @ts-ignore
-  window.whatUa = is;
+  window.whatUa = whatUa;
 }
